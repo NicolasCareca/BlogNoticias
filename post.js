8a0b6f769992692c0389f723bf1315b3abe6d7bf
Nicolas <nicolas10morschel@gmail.com>
2023-08-21 08:14:50 -0300

    terceiro commit

--- post.js
+++ post.js
@@ -26,15 +26,17 @@ function showPost(post) {
         : post.thumbImage;
 
     const postHTML = `
-        <div class="bg-white rounded-lg shadow-md overflow-hidden border-2 border-blue-300">
-            <img class="w-full h-60 object-cover" src="${image}" alt="${post.thumbImageAltText}">
-            <div class="p-4">
-                <h2 class="text-xl font-semibold mb-2">${post.title}</h2>
-                <p class="text-gray-700">${post.description}</p>
-                <p class="mt-2 text-gray-500">Por ${post.profileName}, ${post.postDate}</p>
-          
-            </div>
-        </div>`;
+    <div class="bg-white rounded-lg shadow-md overflow-hidden border-2 border-blue-300">
+    <div class="relative overflow-hidden h-96">
+        <img class="absolute inset-0 w-full h-full object-cover" src="${image}" alt="${post.thumbImageAltText}">
+    </div>
+    <div class="p-4">
+        <h2 class="text-2xl font-semibold mb-2">${post.title}</h2>
+        <p class="text-gray-700">${post.description}</p>
+        <p class="mt-2 text-gray-500">Por ${post.profileName}, ${post.postDate}</p>
+    </div>
+</div>
+`;
     
     postContainer.innerHTML = postHTML;
 }
